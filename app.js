d593f22c0cbd72d9bc01b9aa8cab722470554d47
Fatih Soydan <fatih.soydan@usishi.com>
2012-11-25 16:37:04 +0200

    Add Customer

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/list/:page', router.lists);
+app.get('/add/:page', router.add);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
